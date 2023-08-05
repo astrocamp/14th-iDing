8f7375fe70f62008ac718f8dfb320890febc3f49
justin <justinx0505@gmail.com>
2023-08-05 12:18:38 +0800

    create devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  
   devise_for :restaurants
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
