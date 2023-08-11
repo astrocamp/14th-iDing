bc8df9521b17c8aca079bc2c14a6c84180b6801c
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-11 16:29:32 +0800

    feat: add Customer column: gender(integer)

--- db/migrate/20230810234509_remove_column_from_customer.rb
+++ db/migrate/20230810234509_remove_column_from_customer.rb
@@ -1,8 +1,9 @@
 class RemoveColumnFromCustomer < ActiveRecord::Migration[7.0]
   def change
-    remove_column :customers, :date
-    remove_column :customers, :time
-    remove_column :customers, :purpose
-    remove_column :customers, :note
+    remove_column :customers, :date, :date
+    remove_column :customers, :time, :time
+    remove_column :customers, :purpose, :string
+    remove_column :customers, :note, :string
+    add_column :customers, :gender, :integer
   end
 end
