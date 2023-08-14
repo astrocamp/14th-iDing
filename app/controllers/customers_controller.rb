6c08345c1ea308a0b7ae095f9d7c9c0d962311ea
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-14 14:23:27 +0800

    add Customer CRUD

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,53 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  
+  
+  def index 
+    @customers = Customer.order(id: :desc)
+  end
+
+  def new 
+    @customer = Customer.new
+  end
+
+  def create 
+    @customer = Customer.new(customer_params)
+    if @customer.save 
+      redirect_to customers_path, notice: "訂位新增成功"
+    else
+      render :new 
+    end
+  end
+
+  def show
+  end
+
+  def edit 
+  end
+
+
+  def update 
+    if @customer.update(customer_params)
+      redirect_to customer_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy 
+    @customer.destroy
+    redirect_to customer_path, notice: '刪除成功'
+  end
+
+
+
+  private
+    def customer_params 
+      params.require(:customer).permit(:name, :tel, :email, :gender)
+    end
+
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+end
