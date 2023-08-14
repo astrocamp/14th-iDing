0df420f72490930f1d0ffb7b669bd97b9b64bc2e
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-14 23:22:53 +0800

    feat: add User SoftDelete

--- db/migrate/20230814064308_add_deleted_at_to_customers.rb
+++ db/migrate/20230814064308_add_deleted_at_to_customers.rb
@@ -2,5 +2,7 @@ class AddDeletedAtToCustomers < ActiveRecord::Migration[7.0]
   def change
     add_column :customers, :deleted_at, :datetime
     add_index :customers, :deleted_at
+    add_column :users, :deleted_at, :datetime
+    add_index :users, :deleted_at
   end
 end
