2601667f1e5a51089f1c86ea3a6b12ab88083d56
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-14 23:22:53 +0800

    feat: add User SoftDelete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_paranoid
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :omniauthable, omniauth_providers: [:google_oauth2]
