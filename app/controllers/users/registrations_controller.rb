205adf22308470388806151fab9044e4c8ed2908
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-17 14:16:48 +0800

    fix: rebocop ignore ABC/size & NestMethod

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,10 +25,6 @@ module Users
     # end
 
     # GET /resource/cancel
-    # Forces the session data which is usually expired after sign
-    # in to be expired now. This is useful if the user wants to
-    # cancel oauth signing in/up in the middle of the process,
-    # removing all OAuth session data.
     # def cancel
     #   super
     # end
