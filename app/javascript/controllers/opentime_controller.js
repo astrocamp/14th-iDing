02cf14340cea3c10330eaaa28a0eebd6e3c3786d
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-20 18:33:00 +0800

    change column name from close_time to end_time

--- app/javascript/controllers/opentime_controller.js
+++ app/javascript/controllers/opentime_controller.js
@@ -2,37 +2,42 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="opentime"
 export default class extends Controller {
-  static targets = ["startTime", "closeTime", "submit"];
+  static targets = ["startTime", "endTime", "submit"];
 
   connect() {
-    this.getstartTime();
+    this.checkstartTime();
   }
 
-  getstartTime() {
-    const startTimeHr = this.startTimeTargets[0].value;
-    const startTimeMin = this.startTimeTargets[1].value;
-    this.startTime = new Date(
-      `2000-01-01 ${startTimeHr}:${startTimeMin}:00 +0800`
+  updateTime(targetName) {
+    const [hr, min] = this[targetName + "Targets"].map((input) =>
+      parseInt(input.value, 10)
     );
+    return new Date(2023, 8, 20, hr, min);
+  }
+
+  checkstartTime() {
+    this.startTime = this.updateTime("startTime");
     this.compareTime();
   }
 
-  getcloseTime() {
-    const closeTimeHr = this.closeTimeTargets[0].value;
-    const closeTimeMin = this.closeTimeTargets[1].value;
-    this.closeTime = new Date(
-      `2000-01-01 ${closeTimeHr}:${closeTimeMin}:00 +0800`
-    );
+  checkendTime() {
+    this.endTime = this.updateTime("endTime");
     this.compareTime();
   }
+
+  // compareTime() {
+  //   if (this.startTime < this.endTime) {
+  //     this.submitTarget.classList.remove("disabled-btn");
+  //     this.submitTarget.classList.add("major-btn");
+  //   } else {
+  //     this.submitTarget.classList.add("disabled-btn");
+  //     this.submitTarget.classList.remove("major-btn");
+  //   }
+  // }
 
   compareTime() {
-    if (this.startTime < this.closeTime) {
-      this.submitTarget.classList.remove("disabled-btn");
-      this.submitTarget.classList.add("major-btn");
-    } else {
-      this.submitTarget.classList.add("disabled-btn");
-      this.submitTarget.classList.remove("major-btn");
-    }
+    const TimeCorrect = this.startTime < this.endTime;
+    this.submitTarget.classList.toggle("void-btn", !TimeCorrect);
+    this.submitTarget.classList.toggle("major-btn", TimeCorrect);
   }
 }
