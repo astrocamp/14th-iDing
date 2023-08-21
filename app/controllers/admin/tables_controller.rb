7070c392b5a0c73f599a5aded89966ae4e4ba000
York <york5111@gmail.com>
2023-08-21 12:05:20 +0800

    use rubocop & add basecontroller

--- app/controllers/admin/tables_controller.rb
+++ app/controllers/admin/tables_controller.rb
@@ -1,56 +1,56 @@
-class Admin::TablesController < ApplicationController
+# frozen_string_literal: true
 
-  before_action :authenticate_user!
-  before_action :set_table, only: [:edit, :update, :destroy]
-  before_action :set_restaurant, only: [:index, :new, :create]
+module Admin
+  class TablesController < Admin::BaseController
+    before_action :set_table, only: %i[edit update destroy]
+    before_action :set_restaurant, only: %i[index new create]
 
-  def index
-    @tables = @restaurant.tables.order(id: :asc)
-  end
+    def index
+      @tables = @restaurant.tables.order(:id)
+    end
 
-  def new
-    @table = @restaurant.tables.new
-  end
+    def new
+      @table = @restaurant.tables.new
+    end
 
-  def create
-    @table = @restaurant.tables.new(table_params)
-    
-    if @table.save
-      redirect_to admin_restaurant_tables_path, notice: "桌子建立成功"
-    else
-      render :new
+    def create
+      @table = @restaurant.tables.new(table_params)
+
+      if @table.save
+        redirect_to admin_restaurant_tables_path, notice: '桌子建立成功'
+      else
+        render :new
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @table.update(table_params)
+        redirect_to admin_restaurant_tables_path(@restaurant), notice: '桌子已更新'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @table.destroy
+      redirect_to admin_restaurant_tables_path, notice: '刪除成功'
+    end
+
+    private
+
+    def table_params
+      params.require(:table).permit(:name, :seat_num, :category)
+    end
+
+    def set_table
+      set_restaurant
+      @table = @restaurant.tables.find(params[:id])
+    end
+
+    def set_restaurant
+      @restaurant = current_user.restaurants.find(params[:restaurant_id])
     end
   end
-
-  def edit
-  end
-
-  def update
-    if @table.update(table_params)
-      redirect_to admin_restaurant_tables_path(@restaurant), notice: "桌子已更新"
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @table.destroy
-    redirect_to admin_restaurant_tables_path, notice:'刪除成功'
-  end
-
-  private
-
-  def table_params
-    params.require(:table).permit(:name,:seat_num,:category)
-  end
-
-  def set_table
-    set_restaurant
-    @table = @restaurant.tables.find(params[:id])
-  end
-
-  def set_restaurant
-    @restaurant = current_user.restaurants.find(params[:restaurant_id])
-  end
-  
 end
