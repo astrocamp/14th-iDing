b03f947205c8fcc1d635eee9e4a43c0ca6212766
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-21 14:34:55 +0800

    fix: modify daisyui link position & rubocop check

--- app/controllers/admin/open_times_controller.rb
+++ app/controllers/admin/open_times_controller.rb
@@ -1,60 +1,61 @@
-class Admin::OpenTimesController < ApplicationController
-  before_action :find_restaurant, only: %i[create index]
-  before_action :find_open_time, only: %i[edit update destroy]
+# frozen_string_literal: true
 
+module Admin
+  class OpenTimesController < ApplicationController
+    before_action :find_restaurant, only: %i[create index]
+    before_action :find_open_time, only: %i[edit update destroy]
 
+    def index
+      @open_time = OpenTime.new
+      @open_times = @restaurant.open_times.includes(:restaurant)
+    end
 
-  def index 
-    @open_time = OpenTime.new
-    @open_times = @restaurant.open_times.includes(:restaurant)
-  end
+    def create
+      start_time = build_time('start_time')
+      end_time = build_time('end_time')
 
-  def create
-    start_time = build_time('start_time')
-    end_time = build_time('end_time')
+      return unless start_time < end_time
 
-    if start_time < end_time
       @start_time = @restaurant.open_times.new(opentime_params)
       @start_time.save
     end
-  end
 
-  def edit
-    render layout: 'application'
-  end
+    def edit
+      render layout: 'application'
+    end
 
-  def update
-    return unless @open_time.update(opentime_params)
+    def update
+      return unless @open_time.update(opentime_params)
 
-    redirect_to admin_restaurant_open_times_path(@open_time.restaurant), notice: '已更新時段'
-  end
+      redirect_to admin_restaurant_open_times_path(@open_time.restaurant), notice: '已更新時段'
+    end
 
-  def destroy
-    @open_time.destroy
-  end
+    def destroy
+      @open_time.destroy
+    end
 
+    private
 
-  private
+    def opentime_params
+      params.require(:open_time).permit(:start_time, :end_time)
+    end
 
-  def opentime_params
-    params.require(:open_time).permit(:start_time, :end_time)
-  end
+    def find_restaurant
+      @restaurant = current_user.restaurants.find(params[:restaurant_id])
+    end
 
-  def find_restaurant
-    @restaurant = current_user.restaurants.find(params[:restaurant_id])
-  end
+    def find_open_time
+      @open_time = OpenTime.find(params[:id])
+    end
 
-  def find_open_time
-    @open_time = OpenTime.find(params[:id])
-  end
-
-  def build_time(prefix)
-    Time.new(
-      params[:open_time]["#{prefix}(1i)"].to_i,
-      params[:open_time]["#{prefix}(2i)"].to_i,
-      params[:open_time]["#{prefix}(3i)"].to_i,
-      params[:open_time]["#{prefix}(4i)"].to_i,
-      params[:open_time]["#{prefix}(5i)"].to_i
-    )
+    def build_time(prefix)
+      Time.new(
+        params[:open_time]["#{prefix}(1i)"].to_i,
+        params[:open_time]["#{prefix}(2i)"].to_i,
+        params[:open_time]["#{prefix}(3i)"].to_i,
+        params[:open_time]["#{prefix}(4i)"].to_i,
+        params[:open_time]["#{prefix}(5i)"].to_i
+      )
+    end
   end
 end
