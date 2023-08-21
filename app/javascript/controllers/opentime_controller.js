96cf88b0dec61be13f36d9669ed6faab465129e3
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-22 00:27:44 +0800

    fix: change start_time and end_time input type from time_select to time_field

--- app/javascript/controllers/opentime_controller.js
+++ app/javascript/controllers/opentime_controller.js
@@ -5,28 +5,12 @@ export default class extends Controller {
   static targets = ["startTime", "endTime", "submit"];
 
   connect() {
-    this.checkstartTime();
-  }
-
-  updateTime(targetName) {
-    const [hr, min] = this[targetName + "Targets"].map((input) =>
-      parseInt(input.value, 10)
-    );
-    return new Date(2023, 8, 20, hr, min);
-  }
-
-  checkstartTime() {
-    this.startTime = this.updateTime("startTime");
-    this.compareTime();
-  }
-
-  checkendTime() {
-    this.endTime = this.updateTime("endTime");
     this.compareTime();
   }
 
   compareTime() {
-    const TimeCorrect = this.startTime < this.endTime;
+    const TimeCorrect =
+      this.startTimeTargets[0].value < this.endTimeTargets[0].value;
     this.submitTarget.classList.toggle("void-btn", !TimeCorrect);
     this.submitTarget.classList.toggle("action-btn", TimeCorrect);
     this.submitTarget.disabled = !TimeCorrect;
