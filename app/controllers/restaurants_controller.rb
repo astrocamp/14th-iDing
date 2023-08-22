7a45733e951335a0e6bcaabcbd76b73ba4eeb693
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-22 08:44:22 +0800

    modify route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,15 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show reserve occupied]
+
   def index
   end
-  
-  def show
+
+  def show 
+    @open_time = @restaurant.open_times.order(start_time: :asc)
   end
-  
-  def new
-  end
-  
-  def create
-  end
-  
-  def edit
-  end
-  
-  def update
-  end
-  
-  def destroy
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
