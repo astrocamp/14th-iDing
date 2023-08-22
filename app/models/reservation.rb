bdd06ed504c54af84e8b0ca21fa4270f482a2c06
York <york5111@gmail.com>
2023-08-22 15:29:15 +0800

    add restaurant & reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 class Reservation < ApplicationRecord
-  belongs_to :customer
+  # belongs_to :customer
+  belongs_to :restaurant
 
-  before_validation :generate_serial
+  # before_validation :generate_serial
 
   validates :date, presence: true
   validates :time, presence: true
   validates :adult_num, presence: true, numericality: { greater_than: 0 }
-  validates :kid_num, presence: true
+  validates :kid_num, presence: true, numericality: { greater_than: 0 }
 
   private
 
-  def generate_serial
-    return unless date.present?
+  # def generate_serial
+  #   return unless date.present?
 
-    date_part = date.strftime('%Y%m%d')
-    random_part = rand(100..999)
-    self.serial = "#{date_part}-#{random_part}"
-  end
+  #   date_part = date.strftime('%Y%m%d')
+  #   random_part = rand(100..999)
+  #   self.serial = "#{date_part}-#{random_part}"
+  # end
 end
