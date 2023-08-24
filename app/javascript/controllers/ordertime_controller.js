9c7b65ce4c550d1460fa264b8ee285ff0979fb12
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-24 12:07:59 +0800

    change style from ratiobtn to btn

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -2,15 +2,12 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="ordertime"
 export default class extends Controller {
-  static targets = ["timebtn"];
+  static targets = ["timeBtn", "timeInput"];
   connect() {}
 
   clickTime(e) {
     e.preventDefault();
-    console.log(e.target.value);
     const btnContent = e.target.value;
-    this.timebtnTargets.forEach((btn) => {
-      btn.value = btnContent;
-    });
+    this.timeInputTarget.value = btnContent;
   }
 }
