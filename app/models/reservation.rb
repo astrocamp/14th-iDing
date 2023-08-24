99949a5e536700eba53bf151dbba2ff9d69065cd
Aiko <tysaiw@gmail.com>
2023-08-24 19:43:11 +0800

    feat: booking could be canceled

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,18 +2,8 @@
 
 class Reservation < ApplicationRecord
   belongs_to :restaurant
-
-  # before_validation :generate_serial
   validates :date, presence: true
   validates :time, presence: true
   validates :adult_num, presence: true, numericality: { greater_than: 0 }
   validates :kid_num, presence: true, numericality: { greater_than: 0 }
-
-  # def generate_serial
-  #   return unless date.present?
-
-  #   date_part = date.strftime('%Y%m%d')
-  #   random_part = rand(100..999)
-  #   self.serial = "#{date_part}-#{random_part}"
-  # end
 end
