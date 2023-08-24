752fdfd4e269a4e9ef4803cf086bafe552c79b08
York <york5111@gmail.com>
2023-08-24 12:12:41 +0800

    remove_customer_from_reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_040458) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_040956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_040458) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.string "customer_id"
     t.date "date", null: false
     t.time "time", null: false
     t.integer "adult_num", null: false
