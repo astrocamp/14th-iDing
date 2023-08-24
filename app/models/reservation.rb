1b6446693544ada3b3bb16847c67110d23eb672c
Aiko <tysaiw@gmail.com>
2023-08-24 20:44:45 +0800

    remove numericality validate of kid_num

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,5 +5,5 @@ class Reservation < ApplicationRecord
   validates :date, presence: true
   validates :time, presence: true
   validates :adult_num, presence: true, numericality: { greater_than: 0 }
-  validates :kid_num, presence: true, numericality: { greater_than: 0 }
+  validates :kid_num, presence: true
 end
