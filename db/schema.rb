9ec690eb0493fa723e0e1a85dea7a3b3a2b0b9eb
York <york5111@gmail.com>
2023-08-24 12:21:18 +0800

    remove reservation_phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_040956) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_041948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_040956) do
     t.datetime "updated_at", null: false
     t.bigint "restaurant_id", null: false
     t.string "name", null: false
-    t.string "phone", null: false
     t.string "email"
     t.integer "gender"
     t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
