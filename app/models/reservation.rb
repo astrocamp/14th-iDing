ca732975c5c2108da4d913e02a3c41c4e94cfd4d
Aiko <tysaiw@gmail.com>
2023-08-25 14:53:16 +0800

    modify build controller and kid_num

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,5 +5,5 @@ class Reservation < ApplicationRecord
   validates :date, presence: true
   validates :time, presence: true
   validates :adult_num, presence: true, numericality: { greater_than: 0 }
-  validates :kid_num, presence: true
+  validates :kid_num, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
