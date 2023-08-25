2e5b437d9514a76019656e5737eb7d4a8a7634d8
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-25 09:05:25 +0800

    delete ordertime_controller useless connect()

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="ordertime"
 export default class extends Controller {
   static targets = ["timeBtn", "timeInput"];
-  connect() {}
 
   clickTime(e) {
     e.preventDefault();
