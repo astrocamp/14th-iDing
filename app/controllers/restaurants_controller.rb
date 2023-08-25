bfd56d1db6c5bc29c8940c7a985b2904e7c2c2f8
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-25 10:18:44 +0800

    date can show on restaurant_show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,9 @@
 
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: %i[show]
+  before_action :set_holidays, only: %i[show]
+  before_action :set_weekdays, only: %i[show]
+
 
   def index
     @restaurants = Restaurant.all
@@ -9,6 +12,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @open_time = @restaurant.open_times.order(start_time: :asc)
+    #@chinese_weekdays = ["日", "一", "二", "三", "四", "五", "六"] 
   end
 
   private
@@ -16,4 +20,13 @@ class RestaurantsController < ApplicationController
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+  
+  def set_holidays
+    @holidays = @restaurant.holidays.map(&:dayoff)
+  end
+
+  def set_weekdays
+    @chinese_weekdays = ["日", "一", "二", "三", "四", "五", "六"] 
+  end
+
 end
