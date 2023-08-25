660283c33875b0182760be71c9de67074079da11
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-25 12:24:10 +0800

    feat: resuaurant holiday can appear on show page and book

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -2,11 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="ordertime"
 export default class extends Controller {
-  static targets = ["timeBtn", "timeInput"];
+  static targets = ["timeBtn", "timeInput", "dateInput", "dateBtn"];
+  connect() {}
 
   clickTime(e) {
     e.preventDefault();
     const btnContent = e.target.value;
     this.timeInputTarget.value = btnContent;
+  }
+
+  clickDate(e) {
+    e.preventDefault();
+    const btnContent = e.target.value;
+    this.dateInputTarget.value = btnContent;
   }
 }
