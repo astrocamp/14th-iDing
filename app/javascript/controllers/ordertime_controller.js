018f9ff46b94cb49bab7d8bdfa2d01df5058d9da
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-26 01:05:56 +0800

    fix: modify reservation_range_date logic

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -3,15 +3,22 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="ordertime"
 export default class extends Controller {
   static targets = ["timeBtn", "timeInput", "dateInput", "dateBtn"];
-  connect() {}
-
-  clickTime(e) {
-    e.preventDefault();
-    this.timeInputTarget.value = e.target.value;
-  }
 
   clickDate(e) {
     e.preventDefault();
+    this.dateBtnTargets.forEach((btn) => btn.classList.add("unselect-btn"));
+    this.dateBtnTargets.forEach((btn) => btn.classList.remove("action-btn"));
+    e.target.classList.remove("unselect-btn");
+    e.target.classList.add("action-btn");
     this.dateInputTarget.value = e.target.value;
+  }
+
+  clickTime(e) {
+    e.preventDefault();
+    this.timeBtnTargets.forEach((btn) => btn.classList.add("unselect-btn"));
+    this.timeBtnTargets.forEach((btn) => btn.classList.remove("action-btn"));
+    e.target.classList.remove("unselect-btn");
+    e.target.classList.add("action-btn");
+    this.timeInputTarget.value = e.target.value;
   }
 }
