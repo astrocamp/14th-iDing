7e6178a41fdffec578869f5a3ead86db9b742f8f
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-25 08:57:50 +0800

    feat: holiday CRUD

--- db/migrate/20230824080411_create_holidays.rb
+++ db/migrate/20230824080411_create_holidays.rb
@@ -1,7 +1,7 @@
 class CreateHolidays < ActiveRecord::Migration[7.0]
   def change
     create_table :holidays do |t|
-      t.date :dayoff
+      t.string :dayoff
       t.belongs_to :restaurant, null: false, foreign_key: true
 
       t.timestamps
