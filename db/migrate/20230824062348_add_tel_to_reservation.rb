2ef8f77d623abf52bd6276781c9f382aa1e8f33a
Aiko <tysaiw@gmail.com>
2023-08-26 11:43:12 +0800

    modify tel validation

--- db/migrate/20230824062348_add_tel_to_reservation.rb
+++ db/migrate/20230824062348_add_tel_to_reservation.rb
@@ -1,5 +1,5 @@
 class AddTelToReservation < ActiveRecord::Migration[7.0]
   def change
-    add_column :reservations, :tel, :string
+    add_column :reservations, :tel, :string, null: false
   end
 end
