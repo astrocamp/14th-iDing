22d28d592a49f9c4631043ffdc8246d83abeb380
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-28 19:25:28 +0800

    fix: modify ordertime_controller

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -6,8 +6,10 @@ export default class extends Controller {
 
   clickDate(e) {
     e.preventDefault();
-    this.dateBtnTargets.forEach((btn) => btn.classList.add("unselect-btn"));
-    this.dateBtnTargets.forEach((btn) => btn.classList.remove("action-btn"));
+    this.dateBtnTargets.forEach((btn) => {
+      btn.classList.add("unselect-btn");
+      btn.classList.remove("action-btn");
+    });
     e.target.classList.remove("unselect-btn");
     e.target.classList.add("action-btn");
     this.dateInputTarget.value = e.target.value;
@@ -15,8 +17,10 @@ export default class extends Controller {
 
   clickTime(e) {
     e.preventDefault();
-    this.timeBtnTargets.forEach((btn) => btn.classList.add("unselect-btn"));
-    this.timeBtnTargets.forEach((btn) => btn.classList.remove("action-btn"));
+    this.timeBtnTargets.forEach((btn) => {
+      btn.classList.add("unselect-btn");
+      btn.classList.remove("action-btn");
+    });
     e.target.classList.remove("unselect-btn");
     e.target.classList.add("action-btn");
     this.timeInputTarget.value = e.target.value;
