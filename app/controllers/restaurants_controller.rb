91976c07d09c25a09925addec605357dfd62360a
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-28 18:57:35 +0800

    fix: modify restaurant.helper

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[show]
-  before_action :set_holidays, only: %i[show]
+  before_action :set_restaurant, :set_holidays, :set_timelist, :set_daylist, only: [:show]
 
   def index
     @restaurants = Restaurant.order(:id)
@@ -18,7 +17,25 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  # TimeRange
+  def time_slot
+    @time_period = @restaurant.open_times.reduce([]) { |arr, time| arr.push(time.start_time.to_i..time.end_time.to_i) }
+  end
+
+  def set_timelist
+    time_slot
+    @timerange = @time_period.each_with_object([]) do |time, arr|
+      time.step(@restaurant.reserve_interval.minutes) { |t| arr.push(Time.at(t).utc.strftime('%R')) }
+    end
+  end
+
+  # DateRange
   def set_holidays
     @holidays = @restaurant.holidays.where.not(dayoff: nil).pluck(:dayoff)
+  end
+
+  def set_daylist
+    @end_day = Date.today + @restaurant.bookday_advance.days
+    @daterange = (Date.today..@end_day).select { |date| @holidays.exclude?(date.strftime('%a')) }
   end
 end
