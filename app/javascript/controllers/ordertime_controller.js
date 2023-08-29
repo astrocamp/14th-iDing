4c34949a5e508b22b91eb1e775af95e851218a12
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-08-30 07:29:37 +0800

    wait for fix post 404

--- app/javascript/controllers/ordertime_controller.js
+++ app/javascript/controllers/ordertime_controller.js
@@ -13,6 +13,32 @@ export default class extends Controller {
     e.target.classList.remove("unselect-btn");
     e.target.classList.add("action-btn");
     this.dateInputTarget.value = e.target.value;
+
+    const Token = document
+      .querySelector("meta[name=csrf-token]")
+      .getAttribute("content");
+    const ID = e.currentTarget.dataset.id;
+    const selectedDate = e.target.value;
+    console.log(ID);
+    console.log(selectedDate);
+    fetch(`/restaurants/${ID}/filter_timelist`, {
+      method: "POST", // 或'GET'，根據您的需求
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": Token, // 如果您的後端使用Rails，需要傳遞CSRF令牌
+      },
+      body: JSON.stringify({
+        restaurant_id: ID,
+        date: selectedDate,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   clickTime(e) {
