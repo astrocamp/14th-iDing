eb62e27785e442a506a8355958bd9a10229502d8
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-01 13:11:30 +0800

    add daisyui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     },
   },
   plugins: [
+    require("daisyui"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
