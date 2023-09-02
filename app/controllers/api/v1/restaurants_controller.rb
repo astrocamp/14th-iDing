5f91c9713e2b709e1e1237c907dddcd0ff20d027
justin <justinx0505@gmail.com>
2023-09-02 20:45:09 +0800

    Rubocop

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,8 +1,14 @@
-class Api::V1::RestaurantsController < ApplicationController
-  before_action :authenticate_user!
+# frozen_string_literal: true
 
-  def reservations
-    restaurant = current_user.restaurants.find(params[:id])
-    @reservations = restaurant.reservations.where(date: params[:start]..params[:end])
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+      before_action :authenticate_user!
+
+      def reservations
+        restaurant = current_user.restaurants.find(params[:id])
+        @reservations = restaurant.reservations.where(date: params[:start]..params[:end])
+      end
+    end
   end
 end
