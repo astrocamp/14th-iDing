aa10eae432dd9811ca1adf88bddc797fe2e6fbd8
York <york5111@gmail.com>
2023-09-03 17:00:50 +0800

    add auto assign table name

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -41,6 +41,27 @@ class Reservation < ApplicationRecord
       .order(:date, :time)
   }
 
+  aasm column: :state do
+    state :reserved, initial: true
+    state :keeped, :used, :completed, :cancelled
+
+    event :keep do
+      transitions from: :reserved, to: :keeped
+    end
+
+    event :use do
+      transitions from: %i[reserved keeped], to: :used, after: :update_table_status_to_occupied
+    end
+
+    event :complete do
+      transitions from: :used, to: :completed, after: :update_table_status_to_vacant
+    end
+
+    event :cancel do
+      transitions from: %i[reserved keeped], to: :cancelled
+    end
+  end
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[name tel date restaurant_id]
   end
@@ -58,42 +79,40 @@ class Reservation < ApplicationRecord
     result
   end
 
-  aasm column: :state do
-    state :reserved, initial: true
-    state :keeped, :used, :completed, :cancelled
-
-    event :keep do
-      transitions from: :reserved, to: :keeped
-    end
-
-    event :use do
-      transitions from: %i[reserved keeped], to: :used
-    end
-
-    event :complete do
-      transitions from: :used, to: :completed
-    end
-
-    event :cancel do
-      transitions from: %i[reserved keeped], to: :cancelled
-    end
+  ransacker :name_or_tel_cont do
+    Arel::Nodes::NamedFunction.new('CONCAT_WS', [Arel::Nodes.build_quoted(' '), arel_table[:name], arel_table[:tel]])
   end
-
-  private
 
   def valid_total_guests
     total_guests = adults + kids
-    vacant_table = restaurant.tables.where(status: 'vacant').where('seat_num >= ?', total_guests).first
+    suitable_table = find_suitable_table(total_guests, date, time)
 
-    if vacant_table
-      self.table = vacant_table
+    if suitable_table
+      self.table = suitable_table
     else
+      self.table = nil
       errors.add(:base, '無法找到合適的空桌')
+      false
     end
   end
 
-  ransacker :name_or_tel_cont do
-    Arel::Nodes::NamedFunction.new('CONCAT_WS', [Arel::Nodes.build_quoted(' '), arel_table[:name], arel_table[:tel]])
+  def find_suitable_table(guests, reservation_date, reservation_time)
+    @mealtime = restaurant.mealtime.minutes
+    hour_before = reservation_time - @mealtime + 1
+    hour_after = reservation_time + @mealtime - 1
+
+    restaurant.tables
+              .where('seat_num >= ?', guests)
+              .where.not(id: reserved_table_within_time_range(reservation_date, hour_before, hour_after))
+              .first
+  end
+
+  def reserved_table_within_time_range(date, start_time, end_time)
+    reserved_tables = Reservation
+                      .where(date:, time: start_time..end_time)
+                      .pluck(:table_id)
+
+    reserved_tables.uniq
   end
 
   def update_table_status_to_occupied
@@ -105,15 +124,4 @@ class Reservation < ApplicationRecord
     table = self.table
     table.vacant! if table.present?
   end
-
-  # def valid_total_guests
-  #   total_guests = adults + kids
-  #   vacant_table = restaurant.tables.where(status: 'vacant').where('seat_num >= ?', total_guests).first
-
-  #   if vacant_table
-  #     self.table = vacant_table
-  #   else
-  #     errors.add(:base, '無法找到合適的空桌')
-  #   end
-  # end
 end
