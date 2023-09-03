fa2b6db65a9b769fbb2dc99af4b62f6cdd896591
Aiko <tysaiw@gmail.com>
2023-09-03 23:30:07 +0800

    set restaurant policy

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,36 @@
+class RestaurantPolicy < ApplicationPolicy
+  def index?
+    user && owner?
+  end
+
+  def show?
+    index?
+  end
+
+  def new?
+    index?
+  end
+
+  def create?
+    index?
+  end
+
+  def edit?
+    index?
+  end
+
+  def update?
+    index?
+  end
+
+  def destroy?
+    index?
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
