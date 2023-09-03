f7b42b873fc1df807a238c4397eb71046794758a
Aiko <tysaiw@gmail.com>
2023-09-03 23:14:00 +0800

    set opentime policy

---
+++ app/policies/open_time_policy.rb
@@ -0,0 +1,29 @@
+class OpenTimePolicy < ApplicationPolicy
+  
+  def index?
+    true
+  end
+
+  def create?
+    user && ower?
+  end
+
+  def edit?
+    create?
+  end
+
+  def update?
+    create?
+  end
+
+  def destroy?
+    create?
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
