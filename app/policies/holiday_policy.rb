222937f252904e1ea3761badea5a1406fbf6d6ef
Aiko <tysaiw@gmail.com>
2023-09-03 23:05:29 +0800

    set holiday policy

---
+++ app/policies/holiday_policy.rb
@@ -0,0 +1,17 @@
+class HolidayPolicy < ApplicationPolicy
+
+  def create?
+    user && ower?
+  end
+
+  def destroy?
+    create?
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
