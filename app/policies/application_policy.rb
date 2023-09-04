b93b444a9f99a88f9dcb844316200cea16487240
Aiko <tysaiw@gmail.com>
2023-09-04 23:18:49 +0800

    fix: research result and super_admin btn

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -36,15 +36,13 @@ class ApplicationPolicy
     false
   end
 
-  def owner?
-    record.user == user
-    end
 
   def super_admin?
-    user.role == super_admin
+    user.role == 'super_admin'
   end
 
   class Scope
+    
     def initialize(user, scope)
       @user = user
       @scope = scope
@@ -54,8 +52,12 @@ class ApplicationPolicy
       raise NotImplementedError, "You must define #resolve in #{self.class}"
     end
 
-    private
-
     attr_reader :user, :scope
+  end
+
+  private
+
+  def super_admin?
+    user.role == 'super_admin'
   end
 end
