23fbbde04437cbd9a9e5e1d26fd0eb1e2e870989
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-04 13:16:28 +0800

    add api:v1:table_comtroller

---
+++ app/controllers/api/v1/tables_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TablesController < ApplicationController
+      before_action :set_table, only: [:update_position]
+
+      def update_position
+        new_site_x = params[:site_x]
+        new_site_y = params[:site_y]
+
+        if @table.update(site_x: new_site_x, site_y: new_site_y)
+          render json: { message: '位置以更新' }, status: :ok
+        else
+          render json: { error: '無法更新位置' }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_table
+        @table = Table.find(params[:id])
+      end
+    end
+  end
+end
