a98b2a0ee4106008f88f712d1e9550c650821b1c
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-06 05:48:36 +0800

    import the restaurtant.mealtime

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module TablesHelper
-  def table_image_and_name(table)
+  def table_image_and_name(table, restaurant)
     current_time = Time.current + 8.hour
-    one_hour_ago = current_time - 1.hour
+    one_hour_ago = current_time - restaurant.mealtime.minutes
     reservations = table.reservations.find_by(date: Date.today, time: one_hour_ago..current_time)
 
     if reservations.present?
