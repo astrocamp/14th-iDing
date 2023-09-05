9411c38a151a7d834ee6761ce4cd13bf0f863263
York <york5111@gmail.com>
2023-09-05 17:36:45 +0800

    fix api

--- app/javascript/controllers/check_controller.js
+++ app/javascript/controllers/check_controller.js
@@ -2,26 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="check-in"
 export default class extends Controller {
-  connect() {
-    const { id } = this.element.dataset;
-    this.id = id;
-  }
-  checkIn(e) {
+  static values = { url: String };
+
+  checkIn() {
     const token = document.querySelector("meta[name=csrf-token]").content;
-    const url = `/api/v1/reservations/${this.id}/check_in`;
+    const url = `/api/v1/reservations/${this.urlValue}/check_in`;
 
     fetch(url, {
       method: "PATCH",
       headers: {
         "X-CSRF-Token": token,
       },
-    }).then((resp) => {
-      if (resp.ok) {
-        const stateElement = document.querySelector(
-          `#reservation-state-${this.id}`
-        );
-        stateElement.textContent = "used";
-      }
+    }).then(() => {
+      const stateElement = document.querySelector(
+        `#reservation-state-${this.urlValue}`
+      );
+      return (stateElement.textContent = "used");
     });
   }
 }
--- app/javascript/controllers/leave_controller.js
+++ app/javascript/controllers/leave_controller.js
@@ -2,26 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="leave"
 export default class extends Controller {
-  connect() {
-    const { id } = this.element.dataset;
-    this.id = id;
-  }
+  static values = { url: String };
+
   guestLeave(e) {
     const token = document.querySelector("meta[name=csrf-token]").content;
-    const url = `/api/v1/reservations/${this.id}/leave`;
+    const url = `/api/v1/reservations/${this.urlValue}/leave`;
 
     fetch(url, {
       method: "PATCH",
       headers: {
         "X-CSRF-Token": token,
       },
-    }).then((resp) => {
-      if (resp.ok) {
-        const stateElement = document.querySelector(
-          `#reservation-state-${this.id}`
-        );
-        stateElement.textContent = "completed";
-      }
+    }).then(() => {
+      const stateElement = document.querySelector(
+        `#reservation-state-${this.urlValue}`
+      );
+      stateElement.textContent = "completed";
     });
   }
 }
