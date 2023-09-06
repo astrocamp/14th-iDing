43e56f63209f0de2421e18c9fdb0bb3d361ce6b8
York <york5111@gmail.com>
2023-09-06 20:03:20 +0800

    fix success page RWD add customer's page i18n

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -6,6 +6,6 @@ class SendSmsJob < ApplicationJob
   def perform(reservation)
     sms_client = Twsms2::Client.new(username: ENV.fetch('TWSMS', nil), password: ENV.fetch('TWSMS_PASSWORD', nil))
     sms_client.send_message to: reservation.tel.to_s,
-                            content: "您已預約#{reservation.restaurant.name}。於#{reservation.date}的#{reservation.time.strftime('%R')}。您的訂位資料：http://iding.cc/success_page/#{reservation.slug}"
+                            content: "您已預約#{reservation.restaurant.name}。於#{reservation.date}的#{reservation.time.strftime('%R')}。您的訂位資料：https://iding.cc/success_page/#{reservation.slug}"
   end
 end
