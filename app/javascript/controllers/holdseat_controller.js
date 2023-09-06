dc9a185bd2714bfecae1c010c99c6552a3770551
York <york5111@gmail.com>
2023-09-06 20:03:20 +0800

    fix success page RWD add customer's page i18n

--- app/javascript/controllers/holdseat_controller.js
+++ app/javascript/controllers/holdseat_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
         "X-CSRF-Token": token,
       },
     }).then(() => {
-      const newStateText = "已保留訂位";
+      const newStateText = I18n.t("success.Reservation confirmed");
       const stateElement = document.querySelector(
         `#reservation-state-${this.urlValue}`
       );
