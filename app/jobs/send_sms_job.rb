40b7d43f52a3f0cbea9132f8fc01e799df38967f
Aiko <tysaiw@gmail.com>
2023-09-06 16:26:40 +0800

    fix typo

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -7,7 +7,7 @@ class SendSmsJob < ApplicationJob
     twsms_account = Rails.application.credentials[:twsms][:account]
     twsms_password = Rails.application.credentials[:twsms][:password]
 
-    sms_client = Twsms2::Client.new(username: <%= ENV["TWSMS"] %>, password: <%= ENV["TWSMS_PASSWORD"] %>)
+    sms_client = Twsms2::Client.new(username: ENV["TWSMS"], password: ENV["TWSMS_PASSWORD"])
     sms_client.send_message to: reservation.tel.to_s,
                             content: "您已預約#{reservation.restaurant.name}。於#{reservation.date}的#{reservation.time.strftime('%R')}。您的訂位資料：http://iding.cc/success_page/#{reservation.id}"
   end
