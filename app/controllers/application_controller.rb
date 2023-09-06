b93bb40ec3ac5fbf69715673dafcb27c27456a94
York <york5111@gmail.com>
2023-09-06 10:36:15 +0800

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized # 還要切個畫面
+  around_action :switch_locale
 
   private
 
@@ -18,4 +19,14 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "您無權訪問此頁面。"
     redirect_to(request.referrer || root_path)
   end
+  
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
