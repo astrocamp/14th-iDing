faed79d0f285248eb1250516d950d5ddf6522124
Aiko <tysaiw@gmail.com>
2023-09-07 23:14:10 +0800

    rubocoped

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,13 +3,13 @@
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
     # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
+    def after_resending_confirmation_instructions_path_for(_resource_name)
       sign_in(resource)
       root_path
     end
 
     # The path used after confirmation.
-    def after_confirmation_path_for(resource_name, resource)
+    def after_confirmation_path_for(_resource_name, resource)
       sign_in(resource)
       new_admin_restaurant_path
     end
