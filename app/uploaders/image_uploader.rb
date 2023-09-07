dd417c9e5b6c6c50c0eead28baaed489a1e1c533
justin <justinx0505@gmail.com>
2023-09-08 00:34:50 +0800

    Design preview style for menus

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,8 +3,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
 
   def store_dir
@@ -13,19 +11,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [1260, 540]
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
   def extension_allowlist
-    %w[jpg jpeg gif png]
+    %w[jpg jpeg gif png avif]
   end
 end
