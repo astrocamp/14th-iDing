d33e1dd75aebcaa9f866a6533af423491958eb5a
Aiko <tysaiw@gmail.com>
2023-09-07 13:54:19 +0800

    WIP

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,31 +2,16 @@
 
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
-    # GET /resource/confirmation/new
-    # def new
-    #   super
-    # end
-
-    # POST /resource/confirmation
-    # def create
-    #   super
-    # end
-
-    # GET /resource/confirmation?confirmation_token=abcdef
-    # def show
-    #   super
-    # end
-
-    # protected
-
     # The path used after resending confirmation instructions.
-    # def after_resending_confirmation_instructions_path_for(resource_name)
-    #   super(resource_name)
-    # end
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      sign_in(resource)
+      root_path
+    end
 
     # The path used after confirmation.
-    # def after_confirmation_path_for(resource_name, resource)
-    #   super(resource_name, resource)
-    # end
+    def after_confirmation_path_for(resource_name, resource)
+      sign_in(resource)
+      new_admin_restaurant_path
+    end
   end
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default template_path: 'users/mailer'
+
+  def confirmation_instructions(record, token, opt = {})
+    headers['custom-header'] = 'Bar'
+    opt[:subject] = '加入 iDing 前請驗證您的信箱'
+    opt[:from] = 'noreply@iding.cc'
+    super
+  end
+end
