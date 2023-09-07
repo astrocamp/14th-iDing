2c17407923923646417c05d2699fd26d4dca23a3
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-08 00:14:51 +0800

    add env

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -4,10 +4,10 @@ class SendSmsJob < ApplicationJob
   queue_as :default
 
   def perform(reservation)
-    Rails.application.credentials[:twsms][:account]
-    Rails.application.credentials[:twsms][:password]
+    twsms_account = Rails.application.credentials[:twsms][:account]
+    twsms_password = Rails.application.credentials[:twsms][:password]
 
-    sms_client = Twsms2::Client.new(username: ENV.fetch('TWSMS', nil), password: ENV.fetch('TWSMS_PASSWORD', nil))
+    sms_client = Twsms2::Client.new(username: ENV["TWSMS"], password: ENV["TWSMS_PASSWORD"])
     sms_client.send_message to: reservation.tel.to_s,
                             content: "您已預約#{reservation.restaurant.name}。於#{reservation.date}的#{reservation.time.strftime('%R')}。您的訂位資料：http://iding.cc/success_page/#{reservation.slug}"
   end
