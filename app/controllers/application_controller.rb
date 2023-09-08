2f1bcd569501f87e166096081cb75c0c19eaadb5
York <york5111@gmail.com>
2023-09-08 16:54:36 +0800

    fixdev error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
+    I18n.with_locale(locale, &action)
   end
 
   def default_url_options
