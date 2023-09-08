cf0d345c732ad108a951035e99a27ab2de58fd46
Aiko <tysaiw@gmail.com>
2023-09-08 21:42:54 +0800

    add back switch_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  around_action :switch_locale
 
   private
 
