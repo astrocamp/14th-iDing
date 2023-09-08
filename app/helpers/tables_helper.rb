d1c931a877111505e137e83b2e25cc9e71fda8ab
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-08 11:40:24 +0800

    change name from reservations to reservation

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -4,12 +4,12 @@ module TablesHelper
   def table_image_and_name(table, restaurant)
     current_time = Time.current + 8.hour
     one_hour_ago = current_time - restaurant.mealtime.minutes
-    reservations = table.reservations.find_by(date: Date.today, time: one_hour_ago..current_time)
+    reservation = table.reservations.find_by(date: Date.today, time: one_hour_ago..current_time)
 
-    if reservations.present? && table.status == 'occupied'
+    if reservation.present? && table.status == 'occupied'
       image_name = 'table_occupy.png'
-      reservation_name = reservations.name
-      reservation_time = reservations.time.strftime('%R')
+      reservation_name = reservation.name
+      reservation_time = reservation.time.strftime('%R')
     else
       image_name = 'twinseat.png'
       reservation_name = ''
