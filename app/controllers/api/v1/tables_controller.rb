3cba2aaed10423471fdd9724689abc5dd0f32b63
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-09 23:42:03 +0800

    modify table_controller

--- app/controllers/api/v1/tables_controller.rb
+++ app/controllers/api/v1/tables_controller.rb
@@ -6,10 +6,10 @@ module Api
       before_action :set_table, only: [:update_position]
 
       def update_position
-        new_site_x = params[:site_x]
-        new_site_y = params[:site_y]
+        site_x = params[:site_x]
+        site_y = params[:site_y]
 
-        if @table.update(site_x: new_site_x, site_y: new_site_y)
+        if @table.update(site_x:, site_y:)
           render json: { message: '位置已更新' }, status: :ok
         else
           render json: { error: '無法更新位置' }, status: :unprocessable_entity
