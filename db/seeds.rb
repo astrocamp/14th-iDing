bf1e7e73a6381091e5ff0655a7a2c799a76b75c7
Aiko <tysaiw@gmail.com>
2023-09-09 20:40:25 +0800

    update seed create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
+include I18n
 
 5.times do |i|
-  f_name = Faker::Name.unique.name
   f_email = Faker::Internet.unique.email
   f_password = '123123123'
   f_tel = Faker::PhoneNumber.phone_number
@@ -16,7 +16,7 @@ require 'faker'
     confirmed_at: Time.now
   )
 
-  restaurant = Restaurant.create!(
+  restaurant = Restaurant.create(
     name: f_restaurant_name,
     url: f_restaurant_name,
     tel: f_tel,
@@ -28,14 +28,14 @@ require 'faker'
     bookday_advance: 14
   )
 
-  open_time = OpenTime.create!(
+  open_time = OpenTime.create(
     start_time: Time.parse("11:00 AM"),
     end_time: Time.parse("8:00 PM"),
     restaurant_id: restaurant.id
   )
 
   3.times do |j|
-    table = Table.create!(
+    table = Table.create(
       name: "A#{j + 1}",
       seat_num: rand(2..6),
       status: "available",
@@ -49,52 +49,62 @@ require 'faker'
   end
 end
 
-demo_user = User.create!(
-  name: '小當家',
+demo_user = User.create(
   email: 'cookichiban@gmail.com',
   password: 'cookichiban123',
   confirmed_at: Time.now,
   role: 'super_admin'
 )
+demo_user.save
 
 demo_restaurant = demo_user.restaurants.create(
+  user_id: demo_user.id,
   name: "陽泉酒家",
   url: "ichiban",
-  tel: "02-28825252",
+  tel: "0228825252",
   address: "台北市中正區衡陽路7號5樓",
   description: "一日，法國大廚巴傑魯造訪廣州並貶低中國料理，憤怒的劉昴星與他進行炒飯對決並擊敗了他，巴傑魯心有不甘、縱火燒燬南鮮酒家。為重建餐館，劉昴星參加廣州餃子大賽取得優勝及獎金，餐館重建更名為陽泉酒家。劉昴星依據餃子大賽評審朗文大師的建議參加特級廚師測驗，他在測驗中結交了與自己水準相當的好對手蘭飛鴻，與他一起通過測驗，成為中國史上最年輕的特級廚師。在測驗中發現自己實力尚淺的劉昴星展開修行，他造訪廣西、貴州及雲南的城鎮精進廚藝，旅途中收了四郎為徒。",
-  user_id: demo_user.id,
   reserve_interval: 60,
   mealtime: 90,
-  bookday_advance: 14
+  bookday_advance: 30
 )
 
-demo_opentime = demo_restaurant.opentimes.create(
+demo_opentime = demo_restaurant.open_times.create(
   start_time: Time.parse("11:00 AM"),
   end_time: Time.parse("08:00 PM")
 )
 
-demo_reservation = demo_restaurant.reservations.create(
+demo_table = demo_restaurant.tables.create(
+  name: "A1",
+  seat_num: 4,
+  status: "available",
+  category: 1,
+  site_x: 1,
+  site_y: 1,
+  restaurant_id: demo_restaurant.id
+)
+
+demo_reservation = demo_restaurant.reservations.create!(
   date: Date.parse("2023-10-10"),
   time: Time.parse("06:00 PM"),
   adults: 2,
   kids: 2,
   purpose: "商業聚餐",
   note: '蛋奶素',
-  restaurant_id: demo_restaurant,
+  restaurant_id: demo_restaurant.id,
   name: '解師傅',
   gender: 1,
   tel: '0999999999'
 )
 
-demo_reservation_2 = demo_restaurant.reservations.create(
+demo_reservation_2 = demo_restaurant.reservations.create!(
   date: Date.parse("2023-10-10"),
   time: Time.parse("12:00 PM"),
   adults: 2,
   kids: 0,
   purpose: "慶生",
   note: '當天有壽星',
-  restaurant_id: demo_restaurant,
+  restaurant_id: demo_restaurant.id,
   name: '嘟嘟',
   gender: 2,
   tel: '0988888888'
