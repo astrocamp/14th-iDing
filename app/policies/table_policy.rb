967feb18bdef22de77d86affc4411744d4cc0886
Aiko <tysaiw@gmail.com>
2023-09-09 11:09:03 +0800

    update table policy

--- app/policies/table_policy.rb
+++ app/policies/table_policy.rb
@@ -1,31 +1,7 @@
 # frozen_string_literal: true
 
 class TablePolicy < ApplicationPolicy
-  def index?
-    @user.restaurants.present?
-  end
-
   def show?
-    index?
-  end
-
-  def new?
-    index?
-  end
-
-  def create?
-    index?
-  end
-
-  def edit?
-    index?
-  end
-
-  def update?
-    index?
-  end
-
-  def destroy?
-    index?
+    @user.restaurants.present?
   end
 end
