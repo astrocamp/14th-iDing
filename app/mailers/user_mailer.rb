cded77356481ee5e28e19b9cb72475c577c79111
Aiko <tysaiw@gmail.com>
2023-09-10 23:39:42 +0800

    fix typo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opt = {})
     headers['custom-header'] = 'Bar'
     opt[:subject] = '加入 iDing 前請驗證您的信箱'
-    opt[:from] = 'noreplyr@iding.cc'
+    opt[:from] = 'noreply@iding.cc'
     super
   end
 end
