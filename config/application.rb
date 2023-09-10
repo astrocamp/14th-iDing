88eebcf9fe6e7251501a993117669b0e6bb5e0f8
Aiko <tysaiw@gmail.com>
2023-09-10 21:19:19 +0800

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module IDing
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << ENV["DOMAIN"]
+    config.environment = ENV["RAILS_ENV"] || "development"
   end
 end
