58c9ed88f544c44b1cc3b79c859e3dfa48778f6f
Aiko <tysaiw@gmail.com>
2023-09-13 18:15:20 +0800

    add flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,10 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.css";
+require("flatpickr/dist/themes/dark.css");
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   connect() {
-    flatpickr(".basic", {
-      dateFormat: "Y-m-d",
+    flatpickr(this.element, {
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: "H:i",
+      time_24hr: true,
     });
   }
 }
