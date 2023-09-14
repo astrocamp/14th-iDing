0519eed19e06b81a2a6bbcfe4e350779cb3242c9
Yen <130822984+cookiepingyen@users.noreply.github.com>
2023-09-14 20:41:17 +0800

    rollback timezone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module IDing
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.time_zone = "Taipei"
     config.i18n.default_locale = :tw
     # Configuration for the application, engines, and railties goes here.
     #
