5448201574d2a3937685574536d6b25fc338650d
Aiko <tysaiw@gmail.com>
2023-09-14 14:40:13 +0800

    modify layout and rubocoped

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 module TablesHelper
-  def table_image_and_name(table, _restaurant)
+  def table_image_and_name(table, restaurant)
+    Time.current
+    8.hour
+    restaurant.mealtime.minutes
     reservation = table.reservations.where(date: Date.today).where(state: 'used').order(time: :desc).first
 
     if reservation.present? && table.status == 'occupied'
